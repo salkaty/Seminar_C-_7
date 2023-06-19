caadd3464c3c720f697ef3e89da0568a64059146
Ekaterina <salnikovaekaterina67@gmail.com>
2023-06-19 21:28:35 +0300

    скорректирована функция

--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -14,30 +14,52 @@
 
 void FillArray(int[,] array)
 {
-    int count=0;
+    int count = 0;
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
-            array[i,j] = new Random().Next(-30,31);
-            
+            array[i, j] = new Random().Next(-30, 31);
+
 }
 
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
-        {
+    {
         for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i,j],3}    ");
+            Console.Write($"{array[i, j],3}    ");
         System.Console.WriteLine();
-        }
+    }
 }
 
- void  FindIndex(int[,] array, int count, int index)
- {
-   
-if (index>count)
+void FindIndex(int[,] array, int count, int index, int cols, int rows)
 {
-    System.Console.WriteLine("Такого элемента массива не существует"); 
-}
+
+    if (index > count)
+    {
+        System.Console.WriteLine("Такого элемента массива не существует");
+    }
+    else
+    {
+        int numI = 0;
+        int numJ = 0;
+        while (index > cols)
+        {
+            index = index - cols;
+            numI++;
+        }
+        if (index == cols)
+        {
+            numI++;
+            numJ = cols - 1;
+        }
+        else if (index < cols)
+        {
+            numJ = index - 1;
+        }
+
+        System.Console.WriteLine($"{numI},{numJ} ");
+
+    }
 }
 
 
@@ -47,11 +69,11 @@ System.Console.WriteLine("Введите количество строк");
 int rows = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите количество столбцов");
 int cols = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите номер элемента массива"); 
-int index =Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите номер элемента массива");
+int index = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
-int[,] array = new int [rows, cols];
+int[,] array = new int[rows, cols];
 FillArray(array);
-int count = rows*cols;
+int count = rows * cols;
 PrintArray(array);
-FindIndex(array,count,index);
+FindIndex(array, count, index, cols, rows);
