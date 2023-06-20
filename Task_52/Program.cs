8b480615d32d24397f5a1e9d19ee81b0226f2868
Ekaterina <salnikovaekaterina67@gmail.com>
2023-06-20 13:49:33 +0300

    +

---
+++ Task_52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+//арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().Next(0,101);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    ");
+        System.Console.WriteLine();
+        }
+}
+
+void AveregeToCols(int[,] array, int rows)
+{
+  for (int J = 0; J < array.GetLength(1); j++)
+        {
+        for (int i = 0; i < array.GetLength(0); i++)
+        int sumCols=sumCols+array[i,j];
+            
+        } 
+         Console.Write($"Среднее арифметическое по {J}столбцу равно {sumCols/rows}    ");
+        System.Console.WriteLine();
+}
+
+
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows, cols];
+FillArray(array);
+PrintArray(array);
+AveregeToCols(array,rows);
